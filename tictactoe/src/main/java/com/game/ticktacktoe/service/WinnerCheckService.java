5ce37a61373ae7e78aedcc9e4cbc84cf83ea59d9
Gabriel Kovács <146520052+GabrielK7@users.noreply.github.com>
2024-12-23 17:10:38 +0100

    Update WinnerCheckService changed

--- tictactoe/src/main/java/com/game/ticktacktoe/service/WinnerCheckService.java
+++ tictactoe/src/main/java/com/game/ticktacktoe/service/WinnerCheckService.java
@@ -7,58 +7,67 @@ import org.springframework.stereotype.Service;
 @Service
 public class WinnerCheckService {
     private GameState gameState;
+    private String winner;
 
     @Autowired
     public void setGameState(GameState gameState) {
         this.gameState = gameState;
     }
 
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
     public boolean threeInRow() {
 
         if (!gameState.getField1().isEmpty() && gameState.getField1().equals(gameState.getField2()) &&
-                gameState.getField2().equals(gameState.getField3()) ||
-
-                !gameState.getField4().isEmpty() && gameState.getField4().equals(gameState.getField5()) &&
-                        gameState.getField5().equals(gameState.getField6()) ||
-
-                !gameState.getField7().isEmpty() && gameState.getField7().equals(gameState.getField8()) &&
-                        gameState.getField8().equals(gameState.getField9())) {
-
-            System.out.println("Winner is fond yupiiiii!");
+                gameState.getField2().equals(gameState.getField3())) {
+            setWinner(gameState.getField1());
             return true;
-        } else {
-            return false;
+        } else if (!gameState.getField4().isEmpty() && gameState.getField4().equals(gameState.getField5()) &&
+                gameState.getField5().equals(gameState.getField6())) {
+            setWinner(gameState.getField4());
+            return true;
+        } else if (!gameState.getField7().isEmpty() && gameState.getField7().equals(gameState.getField8()) &&
+                gameState.getField8().equals(gameState.getField9())) {
+            setWinner(gameState.getField4());
+            ;
+            return true;
         }
-
+        return false;
     }
 
-    public boolean threeInColumn(){
+    public boolean threeInColumn() {
         if (!gameState.getField1().isEmpty() && gameState.getField1().equals(gameState.getField4()) &&
-                gameState.getField4().equals(gameState.getField7()) ||
-
-                !gameState.getField2().isEmpty() && gameState.getField2().equals(gameState.getField5()) &&
-                        gameState.getField5().equals(gameState.getField8()) ||
-
-                !gameState.getField3().isEmpty() && gameState.getField3().equals(gameState.getField6()) &&
-                        gameState.getField6().equals(gameState.getField9())) {
-
-            System.out.println("Winner is fond yupiiiii!");
+                gameState.getField4().equals(gameState.getField7())) {
+            setWinner(gameState.getField1());
             return true;
-        } else {
-            return false;
+        } else if (!gameState.getField2().isEmpty() && gameState.getField2().equals(gameState.getField5()) &&
+                gameState.getField5().equals(gameState.getField8())) {
+            setWinner(gameState.getField2());
+            return true;
+        } else if (!gameState.getField3().isEmpty() && gameState.getField3().equals(gameState.getField6()) &&
+                gameState.getField6().equals(gameState.getField9())) {
+            setWinner(gameState.getField3());
+            return true;
         }
+        return false;
     }
-    public boolean threeInDiagonal(){
+
+    public boolean threeInDiagonal() {
         if (!gameState.getField1().isEmpty() && gameState.getField1().equals(gameState.getField5()) &&
-                gameState.getField5().equals(gameState.getField9()) ||
-
-                !gameState.getField3().isEmpty() && gameState.getField3().equals(gameState.getField5()) &&
-                        gameState.getField5().equals(gameState.getField7())) {
-
-            System.out.println("Winner is fond yupiiiii!");
-            return true;
-        } else {
+                gameState.getField5().equals(gameState.getField9())){
+            setWinner(gameState.getField1());
+            return  true;
+        } else if(!gameState.getField3().isEmpty() && gameState.getField3().equals(gameState.getField5()) &&
+                gameState.getField5().equals(gameState.getField7())){
+            setWinner(gameState.getField3());
+            return  true;
+        }
             return false;
         }
-    }
 }
